71ce917837b64a3f0f0439db1a82aac41c1269aa
Philipp Fritsche <ph.fritsche@gmail.com>
2021-01-12 13:45:13 +0100

    chore: initial config

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    extends: [
+        '@ph.fritsche/eslint-config',
+    ],
+}
---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    verbose: true,
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+    ],
+    coveragePathIgnorePatterns: [],
+    testEnvironmentOptions: {
+        resources: 'usable',
+        runScripts: 'dangerously',
+    },
+    testMatch: [
+        '<rootDir>/test/**/*.{js,jsx,ts,tsx}',
+    ],
+    testPathIgnorePatterns: [
+        '/_.*(?<!\\.test\\.[jt]sx?)$',
+    ],
+    transform: {
+        '\\.([jt]sx?)$': 'ts-jest',
+    },
+    transformIgnorePatterns: [
+        '/node_modules/',
+    ],
+    setupFilesAfterEnv: [
+        // "<rootDir>/test/_setup.js",
+    ],
+}
