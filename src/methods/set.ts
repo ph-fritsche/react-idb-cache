3860edf77cf805ac1d6d36843800b48a222eecc1
Philipp Fritsche <ph.fritsche@gmail.com>
2021-01-13 11:12:23 +0100

    feat: set multiple entries

--- src/methods/set.ts
+++ src/methods/set.ts
@@ -1,20 +1,50 @@
-import { set as idbSet } from 'idb-keyval'
+import { setMany } from 'idb-keyval'
 import { reactCache } from '../shared'
 import { cachedObj } from '../useCached'
 
 export function set(
     cache: reactCache,
-    store: Parameters<typeof idbSet>[1],
+    store: Parameters<typeof setMany>[1],
+    record: Record<string, {data: cachedObj['data'], meta?: cachedObj['meta']} | undefined>,
+): void;
+export function set(
+    cache: reactCache,
+    store: Parameters<typeof setMany>[1],
     key: string,
     data: cachedObj['data'],
     meta?: cachedObj['meta'],
+): void;
+export function set(
+    cache: reactCache,
+    store: Parameters<typeof setMany>[1],
+    keyOrRecord: string | Record<string, { data: cachedObj['data'], meta?: cachedObj['meta'] } | undefined>,
+    data?: cachedObj['data'],
+    meta?: cachedObj['meta'],
 ): void {
-    cache[key].obj = {
-        data,
-        meta: {
-            date: new Date(),
-            ...meta,
-        },
-    }
-    idbSet(key, { data, meta }, store)
+    const record = typeof keyOrRecord === 'string' ? { [keyOrRecord]: {data, meta}} : keyOrRecord
+
+    const entries = Object.entries(record).map(([key, obj]): [string, { data: cachedObj['data'], meta: cachedObj['meta'] } | undefined] => {
+        if (typeof obj === 'object') {
+            const {data, meta} = obj
+            return [key, {
+                data,
+                meta: {
+                    date: new Date(),
+                    ...meta,
+                },
+            }]
+        }
+        return [key, undefined]
+    })
+
+    entries.forEach(([key, obj]) => {
+        if (obj) {
+            cache[key].obj = obj
+        } else if(cache[key].promise) {
+            delete cache[key].obj
+        } else {
+            delete cache[key]
+        }
+    })
+    setMany(entries, store)
 }
